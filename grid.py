018e7805610e5affbf1f089fe80ab44c589c7266
David Egan <david@carawebs.com>
2020-06-08 21:15:02 +0100

    First commit

---
+++ grid.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+from PIL import Image, ImageDraw
+
+if __name__ == '__main__':
+    height = 768
+    width = 768
+#    image = Image.new(mode='L', size=(height, width), color=255)
+    image = Image.new(mode='P', size=(height, width), color=255)
+
+    # Draw some lines
+    draw = ImageDraw.Draw(image)
+    y_start = 0
+    y_end = image.height
+    step_size = int(image.width / 16)
+
+    for x in range(0, image.width, step_size):
+        line = ((x, y_start), (x, y_end))
+        draw.line(line, fill=128)
+
+    x_start = 0
+    x_end = image.width
+
+    for y in range(0, image.height, step_size):
+        line = ((x_start, y), (x_end, y))
+        draw.line(line, fill=128)
+
+
+    for x in range(0, 16):
+        for y in range(0, 16):
+            x_coord = (x * step_size) + 3
+            y_coord = (y * step_size) + 3
+            t = x + (y * 16)
+            t_mod = (t % 6) + 1
+            text_color = (0, 0, 255) 
+            draw.text((x_coord, y_coord), str(t), fill='black')
+            draw.text((x_coord + 30, y_coord + 28), str(t_mod), fill=text_color)
+
+    del draw
+
+    image.show()
+
